ba7d9ae2a8c8598c58ad493fc23417e6b2c96551
TheFirstGuy <une202@nyu.edu>
2016-03-27 20:43:30 -0400

    Added comments for muxes in activator skeleton.

---
+++ activattor_skeleton.vhd
@@ -0,0 +1,164 @@
+----------------------------------------------------------------------------------
+-- Company: ECAL
+-- Engineer: Urs Evora
+-- 
+-- Create Date:    18:08:01 03/04/2016 
+-- Design Name: 	
+-- Module Name:    activator_skeleton - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity activator_skeleton is
+PORT(
+--Input
+--Forward Control Signals
+clk: IN STD_LOGIC;
+reset: IN STD_LOGIC;
+--rand_T : IN STD_LOGIC -- not implemented 
+--NOTE: Not sure where these signals originate from
+---rp_pred: IN STD_LOGIC_VECTOR(3 DOWNTO 0 ); -- Vector determining if pred connections exist
+sn_succ: IN STD_LOGIC_VECTOR(3 DOWNTO 0 ); -- Vector determining if succ connections exist
+fwd_pred: IN STD_LOGIC_VECTOR( 3 DOWNTO 1 ); -- Forward pred request signals
+foward: IN STD_LOGIC;  -- Forward activation mode
+
+--Backwards Control Signals
+bck_succ: IN STD_LOGIC_VECTOR( 3 DOWNTO 0); -- backward successors request signals
+backward: IN STD_LOGIC; -- backward activation mode
+update: IN STD_LOGIC; -- update weight control signal
+
+--Forward Data Input Signals
+--Data values from pred for forward activation
+
+---x_pred_0: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+x_pred_1: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+x_pred_2: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+x_pred_3: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+
+--Backward Data Input Signals
+--Data values from succ for back prop
+
+---b_succ_0: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+b_succ_1: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+b_succ_2: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+b_succ_3: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+
+--Output Signals
+y : OUT STD_LOGIC_VECTOR( 19 DOWNTO 0 ); -- output for both forward and back prob
+
+--Forward Control Signals
+fwd_succ: OUT STD_LOGIC_VECTOR( 3 DOWNTO 1); -- Sends request to successors that forward value is ready
+
+--Backwards Control 
+back_pred: OUT STD_LOGIC_VECTOR( 3 DOWNTO 1 ) -- Sends request to preds that back propagation value is ready
+
+);
+end activator_skeleton;
+
+architecture Behavioral of activator_skeleton is
+--ACC_B
+SIGNAL acc_b_out: STD_LOGIC_VECTOR(19 DOWNTO 0 ); -- output of acc_b
+SIGNAL acc_b_in: STD_LOGIC_VECTOR( 19 DOWNTO 0 ); -- input of accumulate B
+--ACC_F
+SIGNAL acc_f_out: STD_LOGIC_VECTOR(19 DOWNTO 0 ); -- output of acc_f
+SIGNAL acc_f_in: STD_LOGIC_VECTOR( 19 DOWNTO 0 ); -- output of mux into ACC_F
+--ACC_W
+SIGNAL acc_w_out: STD_LOGIC_VECTOR(19 DOWNTO 0 ); -- output of acc_w
+--Sel_fwd
+SIGNAL f_sel: STD_LOGIC_VECTOR( 1 DOWNTO 0 ); -- Select signal for forward input MUX
+SIGNAL sel_fwd_reset_m: STD_LOGIC;
+SIGNAL sel_fwd_en_m: STD_LOGIC;  
+--Sel bck
+SIGNAL sel_bck_en_m: STD_LOGIC;
+SIGNAL sel_bck_reset_m: STD_LOGIC;
+SIGNAL b_sel: STD_LOGIC_VECTOR( 1 DOWNTO 0 ); -- selection from select back
+--Mult
+SIGNAL mult_end: STD_LOGIC; -- Result when multiply is finished
+SIGNAL mult_in: STD_LOGIC_VECTOR( 19 DOWNTO 0 ); -- Input for multiplier
+SIGNAL mult_w_in: STD_LOGIC_VECTOR( 19 DOWNTO 0 ); -- Weight or acc_f input for mult
+SIGNAL mult_enable: STD_LOGIC; -- result of ORing sel_fwd_en_in, update_reg, sel_bck_m
+SIGNAL mult_reset: STD_LOGIC;
+--Other
+SIGNAL is_back_prop: STD_LOGIC; -- Result of ANDing backwards and mult_end
+SIGNAL update_reg: STD_LOGIC; -- stores input of update in register for synchronization
+SIGNAL update_and_nupdate: STD_LOGIC; -- output of OR gate for mult reset
+SIGNAL is_fwd: STD_LOGIC; -- Result of ANDING foward and end
+begin
+
+-- Bck_pred 
+is_back_prop <= mult_end AND backward; -- To signal pred for back prop
+back_pred(0) <= rp_pred(0) AND is_back_prop;
+back_pred(1) <= rp_pred(1) AND is_back_prop;
+back_pred(2) <= rp_pred(2) AND is_back_prop;
+back_pred(3) <= rp_pred(3) AND is_back_prop;
+
+--fwd_succ
+is_fwd <= foward AND mult_end;
+fwd_succ(0) <= rn_succ(0) AND is_fwd;
+fwd_succ(1) <= rn_succ(1) AND is_fwd;
+fwd_succ(2) <= rn_succ(2) AND is_fwd;
+fwd_succ(3) <= rn_succ(3) AND is_fwd;
+
+-- Forward input MUX
+WITH f_sel( 1 DOWNTO 0 ) SELECT
+	acc_f_in <= x_pred_0 WHEN "00",
+	x_pred_1 WHEN "01",
+	x_pred_2 WHEN "10",
+	x_pred_3 WHEN "11",
+	x_pred_0 WHEN others;
+
+-- Backward input MUX
+WITH b_sel( 1 DOWNTO 0 ) SELECT
+	acc_b <= b_succ_0 WHEN "00",
+	b_succ_1 WHEN "01",
+	b_succ_2 WHEN "10",
+	b_succ_3 WHEN "11",
+	b_succ_0 WHEN others;
+	
+--Multiply input MUX
+--input ACC_F when foward
+--input ADDout when done activating
+--input ACC_F when updating threshold & backprop
+WITH backward SELECT -- NEEDS TO BE MODIFIED
+	mult_in <= acc_f_out WHEN '0',
+	acc_b_out WHEN '1',
+	acc_f_out WHEN others;
+	
+--Multiply enable control
+	mult_enable <= sel_fwd_en_m OR sel_bck_en_m OR update_reg;
+	
+--Multiply W input MUX
+-- input addout when forward
+-- input addout when done activating
+-- input acc_b when updating threshold & backprop
+WITH update SELECT
+	mult_w_in <= acc_w_out WHEN '0',
+	acc_f_out WHEN '1',
+	acc_w_out WHEN others;
+	
+--Mutliply reset control
+update_and_nupdate <= NOT update_reg AND update;
+mult_reset <= sel_fwd_reset_m OR sel_bck_reset_m OR update_and_nupdate;
+	
+--Update register
+PROCESS(clk, update)
+	BEGIN
+	IF(clk'EVENT AND clk = '1') THEN
+		update_reg <= update;
+	END IF;
+END PROCESS;
+
+
+
+end Behavioral;
