045a0f8512f57b5cc6d402f2e69b5a967886e415
TheFirstGuy <une202@nyu.edu>
2016-04-02 12:22:42 -0400

    Modifed state machine. Tested. Added do file for testing activator

--- activattor_skeleton.vhd
+++ activattor_skeleton.vhd
@@ -26,11 +26,12 @@ PORT(
 --Forward Control Signals
 clk: IN STD_LOGIC;
 reset: IN STD_LOGIC;
+still_fwd: IN STD_LOGIC; --tells activator to go back to forward active. (we are done with back prop)
 --rand_T : IN STD_LOGIC -- not implemented 
 --NOTE: Not sure where these signals originate from
----rp_pred: IN STD_LOGIC_VECTOR(3 DOWNTO 0 ); -- Vector determining if pred connections exist
+rp_pred: IN STD_LOGIC_VECTOR(3 DOWNTO 0 ); -- Vector determining if pred connections exist
 sn_succ: IN STD_LOGIC_VECTOR(3 DOWNTO 0 ); -- Vector determining if succ connections exist
-fwd_pred: IN STD_LOGIC_VECTOR( 3 DOWNTO 1 ); -- Forward pred request signals
+fwd_pred: IN STD_LOGIC_VECTOR( 3 DOWNTO 0 ); -- Forward pred request signals
 foward: IN STD_LOGIC;  -- Forward activation mode
 
 --Backwards Control Signals
@@ -41,7 +42,7 @@ update: IN STD_LOGIC; -- update weight control signal
 --Forward Data Input Signals
 --Data values from pred for forward activation
 
----x_pred_0: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+x_pred_0: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 x_pred_1: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 x_pred_2: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 x_pred_3: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
@@ -49,7 +50,7 @@ x_pred_3: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 --Backward Data Input Signals
 --Data values from succ for back prop
 
----b_succ_0: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
+b_succ_0: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 b_succ_1: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 b_succ_2: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 b_succ_3: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
@@ -58,10 +59,10 @@ b_succ_3: IN STD_LOGIC_VECTOR( 19 DOWNTO 0 );
 y : OUT STD_LOGIC_VECTOR( 19 DOWNTO 0 ); -- output for both forward and back prob
 
 --Forward Control Signals
-fwd_succ: OUT STD_LOGIC_VECTOR( 3 DOWNTO 1); -- Sends request to successors that forward value is ready
+fwd_succ: OUT STD_LOGIC_VECTOR( 3 DOWNTO 0); -- Sends request to successors that forward value is ready
 
 --Backwards Control 
-back_pred: OUT STD_LOGIC_VECTOR( 3 DOWNTO 1 ) -- Sends request to preds that back propagation value is ready
+back_pred: OUT STD_LOGIC_VECTOR( 3 DOWNTO 0 ) -- Sends request to preds that back propagation value is ready
 
 );
 end activator_skeleton;
@@ -100,7 +101,7 @@ SIGNAL add_en: STD_LOGIC; -- enable adder
 SIGNAL add_reset: STD_LOGIC; -- reset adder
 SIGNAL add_ld_a: STD_LOGIC; -- load input a
 SIGNAL add_ld_b: STD_LOGIC; -- load input b
-
+SIGNAL add_out: STD_LOGIC_VECTOR(19 DOWNTO 0);
 --Other
 SIGNAL is_back_prop: STD_LOGIC; -- Result of ANDing backwards and mult_end
 SIGNAL update_reg: STD_LOGIC; -- stores input of update in register for synchronization
@@ -109,6 +110,9 @@ SIGNAL is_fwd: STD_LOGIC; -- Result of ANDING foward and end
 SIGNAL back_ack_all: STD_LOGIC; -- When all pred send ack signal
 SIGNAL mux1_sel: STD_LOGIC; -- Selector for mux 1 (going into mult_in)
 SIGNAL mux2_sel: STD_LOGIC; -- Selector for mux 2 (going into mult_w )
+SIGNAL back_rdy: STD_LOGIC; -- back propagation value ready
+SIGNAL forward_rdy: STD_LOGIC; --forward activation ready
+
 --State Machine
 type s_type is (init, accumulate, fa0, fa1, fa2, fa3, th0, th1, bp0, bp1, bp2, bp3);
 signal state, nextstate: s_type;
@@ -136,8 +140,8 @@ begin
 				WHEN init => nextstate <= accumulate;
 				WHEN accumulate => IF(sel_fwd_en_m = '1') THEN nextstate <= fa0; ELSE nextstate <= accumulate; END IF;
 				WHEN fa0 => nextstate <= fa1;
-				WHEN fa1 => IF(fin = '1') THEN nextstate <= fa2; ELSE nextstate <= fa1; END IF;
-				WHEN fa2 => IF(backward = '1') THEN nextstate <= th0; ELSE nextstate <= fa2; END IF;
+				WHEN fa1 => IF(fin = '1') THEN nextstate <= fa2; ELSE nextstate <= fa0; END IF;
+				WHEN fa2 => IF(backward = '1') THEN nextstate <= th0; ELSIF( still_fwd = '1' ) THEN nextstate <= accumulate; ELSE nextstate <= fa2; END IF;
 				--WHEN fa3 => IF(backward = '1') THEN nextstate <= th0; ELSE nextstate <= fa3; END IF;
 				WHEN th0 => nextstate <= th1; 
 				WHEN th1 => nextstate <= bp0;
@@ -145,6 +149,7 @@ begin
 				WHEN bp1 => nextstate <= bp2;
 				WHEN bp2 => IF( mult_end = '1') THEN nextstate <= bp3; ELSE nextstate <= bp2; END IF; 
 				WHEN bp3 => IF( back_ack_all = '1') THEN nextstate <= init; ELSE nextstate <= bp3; END IF; 
+				WHEN others => nextstate <= init;
 			END CASE;
 	END PROCESS outputFSM;
 	
@@ -153,8 +158,8 @@ begin
 controlSignals: PROCESS( state )
 	BEGIN
 		IF( state = init ) THEN
-			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '1';
+			acc_f_reset0 <= '0';
+			acc_f_reset1 <= '1';
 			mult_reset <= '1';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -165,9 +170,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';
 			mux2_sel <= '0';
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = accumulate ) THEN -- Accumulate inputs, prepare adder with first coeff
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '1';
 			cnt_en <= '0';
 			add_en <= '1';
@@ -178,9 +185,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';
 			mux2_sel <= '0';
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = fa0 ) THEN -- Multiply and increment count
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '1';
 			add_en <= '0';
@@ -191,9 +200,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';--ACC_F
 			mux2_sel <= '1';--ADD
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = fa1 ) THEN-- Add multiply output and new coeffecient
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '1';
@@ -204,9 +215,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';--ACC_F
 			mux2_sel <= '1';--ADD
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = fa2 ) THEN-- Add stores multiply and acc_f updates threshold 
-			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '1';
+			acc_f_reset0 <= '0';
+			acc_f_reset1 <= '1';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '1';
@@ -217,9 +230,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';--ACC_F
 			mux2_sel <= '1';--ADD
+			back_rdy <= '0';
+			forward_rdy <= '1';
 		ELSIF( state = th0 ) THEN-- Multiply<- ACC_F & ACC_B
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -230,9 +245,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';-- ACC_F
 			mux2_sel <= '0';-- ACC_B
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = th1 ) THEN-- ACC_T <- mult/2
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -243,9 +260,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '1';
 			mux1_sel <= '0';-- ACC_F
 			mux2_sel <= '0';-- ACC_B
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = bp0 ) THEN-- MULT <- ADD & ADD
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -256,9 +275,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '1';-- ADD
 			mux2_sel <= '1';-- ADD
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = bp1 ) THEN-- ACC_F <- (1 - x)
-			acc_f_reset1 <= '1';
-			acc_f_reset2 <= '0';
+			acc_f_reset0 <= '1';
+			acc_f_reset1 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -269,9 +290,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';-- ACC_F
 			mux2_sel <= '0';-- ACC_B
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = bp2 ) THEN-- MULT <- ACC_F & ACC_B
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -282,9 +305,11 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';-- ACC_F
 			mux2_sel <= '0';-- ACC_B
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		ELSIF( state = bp3) THEN -- MULT WAIT
+			acc_f_reset0 <= '0';
 			acc_f_reset1 <= '0';
-			acc_f_reset2 <= '0';
 			mult_reset <= '0';
 			cnt_en <= '0';
 			add_en <= '0';
@@ -295,6 +320,23 @@ controlSignals: PROCESS( state )
 			acc_t_en <= '0';
 			mux1_sel <= '0';-- ACC_F
 			mux2_sel <= '0';-- ACC_B
+			back_rdy <= '1';
+			forward_rdy <= '0';
+		ELSE -- Default to init stage
+			acc_f_reset0 <= '0';
+			acc_f_reset1 <= '1';
+			mult_reset <= '1';
+			cnt_en <= '0';
+			add_en <= '0';
+			add_reset <= '1';
+			add_ld_a <= '0';
+			add_ld_b <= '0';
+			mult_enable <= '0';
+			acc_t_en <= '0';
+			mux1_sel <= '0';
+			mux2_sel <= '0';
+			back_rdy <= '0';
+			forward_rdy <= '0';
 		END IF;
 
 	
@@ -302,7 +344,7 @@ END PROCESS controlSignals;
 	
 
 -- Bck_pred 
-is_back_prop <= mult_end AND backward; -- To signal pred for back prop
+is_back_prop <= back_rdy AND backward; -- To signal pred for back prop
 back_pred(0) <= rp_pred(0) AND is_back_prop;
 back_pred(1) <= rp_pred(1) AND is_back_prop;
 back_pred(2) <= rp_pred(2) AND is_back_prop;
@@ -310,10 +352,10 @@ back_pred(3) <= rp_pred(3) AND is_back_prop;
 
 --fwd_succ
 is_fwd <= foward AND mult_end;
-fwd_succ(0) <= rn_succ(0) AND is_fwd;
-fwd_succ(1) <= rn_succ(1) AND is_fwd;
-fwd_succ(2) <= rn_succ(2) AND is_fwd;
-fwd_succ(3) <= rn_succ(3) AND is_fwd;
+fwd_succ(0) <= sn_succ(0) AND is_fwd;
+fwd_succ(1) <= sn_succ(1) AND is_fwd;
+fwd_succ(2) <= sn_succ(2) AND is_fwd;
+fwd_succ(3) <= sn_succ(3) AND is_fwd;
 
 -- Forward input MUX
 WITH f_sel( 1 DOWNTO 0 ) SELECT
@@ -325,7 +367,7 @@ WITH f_sel( 1 DOWNTO 0 ) SELECT
 
 -- Backward input MUX
 WITH b_sel( 1 DOWNTO 0 ) SELECT
-	acc_b <= b_succ_0 WHEN "00",
+	acc_b_in <= b_succ_0 WHEN "00",
 	b_succ_1 WHEN "01",
 	b_succ_2 WHEN "10",
 	b_succ_3 WHEN "11",
