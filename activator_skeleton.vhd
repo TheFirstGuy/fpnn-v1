bd20a37eaed950647c147a3321b968e565959bd8
omair94 <omair1994@gmail.com>
2016-04-10 17:48:33 -0400

    Added files via upload

--- activator_skeleton.vhd
+++ activator_skeleton.vhd
@@ -155,7 +155,9 @@ component SELECTOR is
 			forward: std_logic;
 			r, reqs: in std_logic_vector(3 downto 0);
 				en_a: out std_logic;
-				 sel: out std_logic_vector(1 downto 0)
+				 sel: out std_logic_vector(1 downto 0);
+			  en_m : out std_logic;
+		  	  res_m: out std_logic
 		);
 end component;
 
