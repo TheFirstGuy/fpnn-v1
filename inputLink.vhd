7ec55d60f47515403fb746ff4784f946b2cfda0d
TheFirstGuy <une202@nyu.edu>
2016-04-18 10:06:11 -0400

    fixed neuron backprop connections. Added output neuron. Added input link

---
+++ inputLink.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    08:03:36 04/18/2016 
+-- Design Name: 
+-- Module Name:    inputLink - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity inputLink is
+PORT(
+	--Data
+	y: OUT STD_LOGIC_VECTOR(19 DOWNTO 0);
+	-- UART IN
+	uart_in: IN STD_LOGIC_VECTOR(19 DOWNTO 0); 
+	-- Control Signal
+	fwd_succ: IN STD_LOGIC --Forward request signal
+	
+);
+end inputLink;
+
+architecture Behavioral of inputLink is
+begin
+y <= uart_in;
+fwd_succ<='1';
+
+end Behavioral;
+
--- o_neuron.vhd
+++ o_neuron.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    13:47:06 04/10/2016 
+-- Create Date:    08:08:48 04/18/2016 
 -- Design Name: 
--- Module Name:    neuron - Behavioral 
+-- Module Name:    o_neuron - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -20,16 +20,9 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+--Neuron which does not have a north link. Output comes directly from activator
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity neuron is
+entity o_neuron is
 PORT(
 	--Control
 	clk: IN STD_LOGIC;
@@ -39,6 +32,12 @@ PORT(
 	backward: IN STD_LOGIC;
 	update: IN STD_LOGIC;
 	reset: IN STD_LOGIC;
+	--Input Activator connections
+	error: IN STD_LOGIC_VECTOR(19 DOWNTO 0); --From error module
+	error_req: IN STD_LOGIC;
+	--Output Activator connections
+	act_out: OUT STD_LOGIC_VECTOR(19 DOWNTO 0);
+	act_fwd_r: OUT STD_LOGIC;
 	--Input Link Connections
 	--South
 	south_fdata_in: IN STD_LOGIC_VECTOR(19 DOWNTO 0); -- Forward data
@@ -60,14 +59,6 @@ PORT(
 	wa_in_r, we_in_r: IN STD_LOGIC;
 	wa_out_r, we_out_r: OUT STD_LOGIC;
 	--Output Link Connections
-	--North
-	north_fdata_out: OUT STD_LOGIC_VECTOR(19 DOWNTO 0);
-	north_bdata_in0: IN STD_LOGIC_VECTOR(19 DOWNTO 0);
-	north_bdata_in1: IN STD_LOGIC_VECTOR(19 DOWNTO 0);
-	north_bdata_in2: IN STD_LOGIC_VECTOR(19 DOWNTO 0);
-	north_bdata_in3: IN STD_LOGIC_VECTOR(19 DOWNTO 0);
-	north_r: OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-	north_br: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
 	--East
 	east_fdata_out: OUT STD_LOGIC_VECTOR(19 DOWNTO 0); -- Forward data out of neuron
 	east_bdata_in0: IN STD_LOGIC_VECTOR(19 DOWNTO 0); -- backward data into neuron
@@ -85,9 +76,9 @@ PORT(
 	west_r: OUT STD_LOGIC_VECTOR(3 DOWNTO 0); -- Output request signals
 	west_br: IN STD_LOGIC_VECTOR(3 DOWNTO 0) -- backward input request signals
 );
-end neuron;
+end o_neuron;
 
-architecture Behavioral of neuron is
+architecture Behavioral of o_neuron is
 
 component activator_skeleton
 PORT(
@@ -178,28 +169,27 @@ back_pred: OUT STD_LOGIC_VECTOR( 3 DOWNTO 0 ) -- Sends request to preds that bac
 
 );
 end component;
-
 --Activator signals
-SIGNAL north_back_r: STD_LOGIC;
-SIGNAL act_out: STD_LOGIC_VECTOR(19 DOWNTO 0);
-SIGNAL act_fwd_r: STD_LOGIC_VECTOR(3 DOWNTO 0);
+--SIGNAL act_out: STD_LOGIC_VECTOR(19 DOWNTO 0);
+--SIGNAL act_fwd_r: STD_LOGIC_VECTOR(3 DOWNTO 0);
 --West Signals
 SIGNAL west_y: STD_LOGIC_VECTOR(19 DOWNTO 0);
 
 --North Signals
-SIGNAL north_y: STD_LOGIC_VECTOR(19 DOWNTO 0);
+--SIGNAL north_y: STD_LOGIC_VECTOR(19 DOWNTO 0);
 
 --east Signals
 SIGNAL east_y: STD_LOGIC_VECTOR(19 DOWNTO 0);
 --NULLS
 SIGNAL ground: STD_LOGIC_VECTOR(7 DOWNTO 0);
 begin
-	--PORT MAP
+
+--PORT MAP
 	ACT: activator_skeleton PORT MAP(clk=>clk, reset=>reset, still_fwd=>still_fwd, 
 	fwd_pred(0)=>wa_in_r, fwd_pred(1)=>sa_in_r, fwd_pred(2)=>ea_in_r, fwd_pred(3)=>'0',
-	foward=>forward, bck_succ(0)=>north_back_r, bck_succ(3 DOWNTO 1)=>"000", backward=>backward,
+	foward=>forward, bck_succ(0)=>error_req, bck_succ(3 DOWNTO 1)=>"000", backward=>backward,
 	broadcast=>broadcast, x_pred_0=>west_fdata_in, x_pred_1=>south_fdata_in,
-	x_pred_2=>east_fdata_in, b_succ_1=>north_y, y=>act_out, fwd_succ=>act_fwd_r,
+	x_pred_2=>east_fdata_in, b_succ_1=>error, y=>act_out, fwd_succ=>act_fwd_r,
 	back_pred(0)=> wa_out_r, back_pred(1)=>sa_out_r, back_pred(2)=>ea_out_r, back_pred(3)=>ground(0));
 	
 	
@@ -210,27 +200,11 @@ begin
 	b_succ_3=>west_bdata_in3, y=>west_y, fwd_succ=>west_r, back_pred(0)=>ew_out_r, back_pred(1)=>sw_out_r,
 	back_pred(3 DOWNTO 2)=>ground(2 DOWNTO 1));
 	
-	NL: link_skeleton PORT MAP( clk=>clk, reset=>reset, fwd_pred(0)=>act_fwd_r(0), fwd_pred(3 DOWNTO 1)=>"000",
-	foward=>forward, bck_succ=>north_br, backward=>backward, update=>update, broadcast=>broadcast, x_pred_0=>act_out,
-	x_pred_1=>open, x_pred_2=>open, x_pred_3=>open, b_succ_0=>north_bdata_in0, b_succ_1=>north_bdata_in1,
-	b_succ_2=>north_bdata_in2, b_succ_3=>north_bdata_in3, y=>north_y, fwd_succ=>north_r, back_pred(0)=>north_back_r,
-	back_pred(3 DOWNTO 1)=>ground(5 DOWNTO 3));
-	
 	EL: link_skeleton PORT MAP( clk=>clk, reset=>reset, fwd_pred(0)=>we_in_r, fwd_pred(1)=>se_in_r, 
 	fwd_pred(3 DOWNTO 2)=>"00", foward=>forward, bck_succ=>east_br, backward=>backward, update=>update,
 	broadcast=>broadcast, x_pred_0=>west_fdata_in, x_pred_1=>south_fdata_in, x_pred_2=>open, x_pred_3=>open,
 	b_succ_0=>east_bdata_in0, b_succ_1=>east_bdata_in1, b_succ_2=>east_bdata_in2, b_succ_3=>east_bdata_in3, y=>east_y,
 	fwd_succ=>east_r, back_pred(0)=>we_out_r, back_pred(1)=>se_out_r, back_pred(3 DOWNTO 2)=>ground(7 DOWNTO 6));
 	
-
-	west_fdata_out<=west_y;
-	ew_bdata_out<=west_y;
-	sw_bdata_out<=west_y;
-	we_bdata_out<=east_y;
-	se_bdata_out<=east_y;
-	ea_bdata_out<=act_out;
-	wa_bdata_out<=act_out;
-	sa_bdata_out<=act_out;
-	north_fdata_out<=north_y;
 end Behavioral;
 
