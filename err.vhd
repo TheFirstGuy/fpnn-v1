3ab571a7a1c8a8e47ebd84c337338a92168e3946
TheFirstGuy <une202@nyu.edu>
2016-05-06 14:37:00 -0400

    Fixed error module to have fwd requests and back requests

--- err.vhd
+++ err.vhd
@@ -35,21 +35,34 @@ use IEEE.STD_LOGIC_SIGNED.ALL;
 entity err is
     Port ( clk : in std_logic;  --Clock
            rst : in std_logic;  --Reset
+			  broadcast : in std_logic; -- broadcast
+			  rslt_valid : in std_logic; -- result ready
            rslt : in std_logic_vector(19 downto 0); --Calculated Result
            c_val : in std_logic_vector(19 downto 0);    --Classification Value
-           err : out std_logic_vector(19 downto 0));    --Calculated Error
+           err : out std_logic_vector(19 downto 0);    --Calculated Error
+			  err_valid: out std_logic); -- error output valid
 end err;
 
 architecture Behavioral of err is
     signal err_val : std_logic_vector(19 downto 0);
 begin
-    calc_err : process(clk, rst, rslt, c_val)
+    calc_err : process(clk, rst, rslt, c_val, rslt_valid, broadcast)
     begin
         if (clk'event and clk =  '1') then
             if (rst = '1') then
                 err_val <= (others => '0');
-            else
-                err_val <= c_val - rslt;    --Calculate Error (Classification - Result)
+					 err_valid <= '0';
+				else
+					if( broadcast = '1' or rslt_valid = '1') then -- broadcast 
+						 err_valid <= '1';
+					else
+						 err_valid <= '0';
+					end if;
+					if( rslt_valid = '1')then
+						err_val <= c_val - rslt;    --Calculate Error (Classification - Result)
+					else
+						err_val <= err_val;
+					end if;
             end if;
         end if;
     end process calc_err;
