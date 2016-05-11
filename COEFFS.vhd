94272bdbbaf2e507fab2d20e77c5519384d6e05b
omair94 <omair1994@gmail.com>
2016-05-11 15:14:42 -0400

    Revert "everything"
    This reverts commit 9159965b219097c74fdb6be92ccad7bd7cc35272.

--- COEFFS.vhd
+++ COEFFS.vhd
@@ -63,7 +63,7 @@ SIGNAL x_addr_3: BOOLEAN; -- x < -1.92033
 TYPE rom IS ARRAY (0 TO 2) OF STD_LOGIC_VECTOR(19 DOWNTO 0);
 --Initialize constant arrays
 -- 0.2603607177734375x^2 -0.999969482421875x + 0
-CONSTANT neg: rom:=rom'(X"00000", "11110000000000000001","00000100001010100111");
+CONSTANT neg: rom:=rom'(X"00000", "00001111111111111110","00000100001010100111");
 -- -0.2603607177734375x^2 +0.999969482421875x + 0
 CONSTANT pos: rom:=rom'(X"00000", "00001111111111111110","11111011110101011001");
 -- 0.9601593017578125
@@ -78,42 +78,43 @@ x_addr_1 <= SIGNED(address) > zero;
 x_addr_2 <= SIGNED(address) > min;
 x_addr_3 <= SIGNED(address) < min;
 
---xs <= To_Std_Logic(x_addr_0) & To_Std_Logic(x_addr_1) & To_Std_Logic(x_addr_2) & To_Std_Logic(x_addr_3);
+xs <= To_Std_Logic(x_addr_0) & To_Std_Logic(x_addr_1) & To_Std_Logic(x_addr_2) & To_Std_Logic(x_addr_3);
 --Select constant
---PROCESS( xs, degree )
---BEGIN
---	case xs is
---	--x > 1.920318603515625
---		when "1000"=>coeff <= mpos(CONV_INTEGER(degree));
---		-- 0 < x <= 1.920318603515625
---		when "0100"=>coeff <= pos(CONV_INTEGER(degree));
---		-- -1.920318603515625 < x < 0
---		when "0010"=>coeff <= neg(CONV_INTEGER(degree));
---		-- x < -1.920318603515625
---		when "0001"=>coeff <= npos(CONV_INTEGER(degree));
---		when others=>coeff <= mpos(CONV_INTEGER(degree));
---	END CASE;
---END PROCESS;
 
-
-PROCESS( x_addr_0, x_addr_1, x_addr_2, x_addr_3, degree )
+PROCESS( xs, degree )
 BEGIN
---x > 1.920318603515625
-IF( x_addr_0) THEN 
-	coeff <= mpos(CONV_INTEGER(degree));
--- 0 < x <= 1.920318603515625
-ELSIF( x_addr_1 AND NOT x_addr_0 ) THEN
-	coeff <= pos(CONV_INTEGER(degree));
--- -1.920318603515625 < x < 0
-ELSIF( x_addr_2 AND NOT x_addr_3 ) THEN
-	coeff <= neg(CONV_INTEGER(degree));
--- x < -1.920318603515625
-ELSIF( x_addr_3 ) THEN
-	coeff <= npos(CONV_INTEGER(degree));
-ELSE
-	coeff <= mpos(CONV_INTEGER(degree));
-END IF;
+	case xs is
+	--x > 1.920318603515625
+		when "1100"=>coeff <= mpos(CONV_INTEGER(degree));
+		-- 0 < x <= 1.920318603515625
+		when "0110"=>coeff <= pos(CONV_INTEGER(degree));
+		-- -1.920318603515625 < x < 0
+		when "0010"=>coeff <= neg(CONV_INTEGER(degree));
+		-- x < -1.920318603515625
+		when "0001"=>coeff <= npos(CONV_INTEGER(degree));
+		when others=>coeff <= mpos(CONV_INTEGER(degree));
+	END CASE;
 END PROCESS;
+
+
+--PROCESS( x_addr_0, x_addr_1, x_addr_2, x_addr_3, degree )
+--BEGIN
+----x > 1.920318603515625
+--IF( x_addr_0) THEN 
+--	coeff <= mpos(CONV_INTEGER(degree));
+---- 0 < x <= 1.920318603515625
+--ELSIF( x_addr_1 AND NOT x_addr_0 ) THEN
+--	coeff <= pos(CONV_INTEGER(degree));
+---- -1.920318603515625 < x < 0
+--ELSIF( x_addr_2 AND NOT x_addr_3 ) THEN
+--	coeff <= neg(CONV_INTEGER(degree));
+---- x < -1.920318603515625
+--ELSIF( x_addr_3 ) THEN
+--	coeff <= npos(CONV_INTEGER(degree));
+--ELSE
+--	coeff <= mpos(CONV_INTEGER(degree));
+--END IF;
+--END PROCESS;
 
 end Behavioral;
 
