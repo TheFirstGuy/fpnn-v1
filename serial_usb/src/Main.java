e448ccbc8acccee573cfa8c1af4f5f922c8796f8
StevenL94 <steven_lee_13@hotmail.com>
2016-05-14 12:48:57 -0400

    Testing

--- serial_usb/src/Main.java
+++ serial_usb/src/Main.java
@@ -162,7 +162,7 @@ public class Main {
     public static class SerialWrite implements Runnable {
 //        Send Data
         OutputStream out;
-        char[] buffer = new char[1];
+        char[] buffer = new char[5];
         int j = 0;
 
         public SerialWrite(OutputStream out ) {
@@ -180,16 +180,17 @@ public class Main {
                     System.out.println(word);
                     for (int i = 0; i < word.length(); i++) {
                         char c = word.charAt(i);
-                        if (cnt < 1) {
+                        this.out.write(c);
+                        if (cnt < 5) {
                             this.out.write(c);
                         }
                         else {
-                            if (j < 1) {
+                            if (j < 5) {
                                 buffer[j] = c;
                                 j++;
                             }
                             else {
-                                for (int k = 0; k < 1; k++) {
+                                for (int k = 0; k < 5; k++) {
                                     this.out.write(buffer[k]);
                                     j = 0;
                                     cnt = 0;
